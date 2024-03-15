360bfb5a20f278862246ccd9b52e8dbfa3962d8a
linzy-vosena <vihendalinzi@gmail.com>
2024-03-14 19:20:23 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguements.".format(x))
+    elif x == 1:
+        print("{} arguements:".format(x))
+    else:
+        print("{} arguements:".format(x))
+
+    for q in range(x):
+        print("{} : {:s}".format(q + 1, argv[q +1]))
